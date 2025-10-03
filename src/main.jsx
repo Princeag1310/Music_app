cfcec9cf162ac3d228c52e66458ac256500e6aac
Sanskriti Mishra <sanskritimishra2611@gmail.com>
2025-10-03 17:01:14 +0530

    Clean up commented code and remove unwanted code

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,4 @@
-// import App from "./App.jsx";
 
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-
-// createRoot(document.getElementById("root")).render(<App />);
-
-
-// src/main.jsx - Update your main file
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
