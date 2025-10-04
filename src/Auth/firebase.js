d7f2748241931590e65c491cfd0f0b627c93580e
Sanskriti Mishra <sanskritimishra2611@gmail.com>
2025-10-04 17:25:20 +0530

    Updated Firebase config to use environment variables

--- src/Auth/firebase.js
+++ src/Auth/firebase.js
@@ -5,15 +5,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  // Ensure these variable names exactly match your .env file
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
