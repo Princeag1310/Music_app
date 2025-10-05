b211288417e3c3388f90416900d392957acfbaa5
Owaise <owaiseimdad@gmail.com>
2025-10-05 14:38:30 +0530

    Fixed the UI discrepencies and made a similar UI as albums above.

--- src/components/Artist/artists.jsx
+++ src/components/Artist/artists.jsx
@@ -17,30 +17,28 @@ function RandomArtists({ search }) {
   return (
     <>
       {artists && (
-        <div className="mt-4 mb-[15dvh] w-[95vw] sm:w-full border rounded-xl p-4 shadow-lg">
+        <div className="mt-6 w-[95vw] sm:w-full sm:mt-8 border p-4 rounded-xl shadow-lg">
           <h2 className="text-2xl font-bold mb-4">Artists</h2>
-          <div className="flex space-x-4 px-4 overflow-x-scroll">
+          <div className="flex space-x-4 overflow-x-scroll">
             {artists?.map((artist, i) => (
               <div
                 key={i}
-                className="flex-none w-40 group mb-4 rounded-lg bg-secondary"
+                className="bg-secondary rounded-2xl p-3 sm:p-4 flex flex-col items-center flex-shrink-0 cursor-pointer hover:scale-105 transition-transform"
                 onClick={() => handleClick(artist.id)}
               >
-                <div className="flex flex-col items-center aspect-square mb-2 overflow-hidden rounded-lg">
                   {artist?.image[2] ? (
                     <img
                       src={artist?.image[2].url}
                       alt={artist?.name}
                       loading="lazy"
-                      className="w-32 h-32 object-cover rounded-lg mt-2 transition-transform duration-200 group-hover:scale-105"
+                      className="w-24 h-24 sm:w-32 sm:h-32 object-cover rounded-lg mb-2"
                     />
                   ) : (
                     <div className="w-32 h-32 bg-gray-200 flex items-center justify-center rounded-lg">
                       <span>No Image</span>
                     </div>
                   )}
-                </div>
-                <h3 className="text-sm font-medium truncate w-32 text-center">
+                                <h3 className="text-center w-32 sm:w-32 text-xs sm:text-sm truncate ">
                   {artist?.name}
                 </h3>
               </div>
